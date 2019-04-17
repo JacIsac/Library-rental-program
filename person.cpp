d9e4099bdb06488018cbc07462663a975701bf2d
JacIsac <36177351+JacIsac@users.noreply.github.com>
2019-04-16 18:50:24 -0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,1 +1,36 @@
+//Jacqueline Cardenas
+//Section 02
+#include "person.h"
+
+Person::Person(int cardNo, bool act, string fName, string lName) {
+    // complete constructor
+    cardID=cardNo;
+    active=act;
+    firstName=fName;
+    lastName=lName;
+}
+
+string Person::getFirstName() {
+    return firstName; // complete
+}
+
+string Person::getLastName() {
+    return lastName; // complete
+}
+
+int Person::getId() {
+    return cardID; // complete
+}
+
+void Person::setActive(bool act) {
+  active=act;
+} // complete
+
+bool Person::isActive() {
+    return true; // complete
+}
+
+string Person::fullName() {
+    return firstName +" "+ lastName; // complete
+}
 
